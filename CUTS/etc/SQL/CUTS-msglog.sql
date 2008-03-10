654ededaeba514f6fefa74505e8b44468fd5706d
James H. Hill <hillj@cs.iupui.edu>
2008-03-10 15:32:35 +0000

    Mon Mar 10 15:29:02 UTC 2008  James H. Hill  <hillj@isis.vanderbilt.edu>
    git-svn-id: https://svn.cs.iupui.edu/repos/CUTS/trunk@1845 9fe22f9e-a3cf-4311-a215-5c011952144b

---
+++ CUTS/etc/SQL/CUTS-msglog.sql
@@ -0,0 +1,36 @@
+--
+-- @file        CUTS-msglog.sql
+--
+-- $Id: CUTS-msglog.sql 1785 2008-02-04 23:12:28Z hillj $
+--
+-- @author      James H. Hill
+--
+
+--
+-- Create the test table. This table contains the test
+-- registration information to map/manage the information
+-- contained in the exeuction_time table correctly.
+--
+
+CREATE TABLE IF NOT EXISTS cuts.msglog
+(
+  test_number     INT             NOT NULL,
+  hostid          INT             NOT NULL,
+  msgtime         TIMESTAMP       NOT NULL DEFAULT NOW(),
+  severity        INT             NOT NULL,
+  message         VARCHAR (255)   NOT NULL,
+
+  -- set the contraints for the table
+  PRIMARY KEY (test_number, msgtime),
+  
+  FOREIGN KEY (test_number) REFERENCES cuts.tests (test_number)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE,
+  FOREIGN KEY (hostid) REFERENCES cuts.ipaddr_host_map (hostid)
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+);
+
+DELIMITER //
+
+DELIMITER ;
