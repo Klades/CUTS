53170582e39e14dcf87db5371eea7f7fa66c8c20
James H. Hill <hillj@cs.iupui.edu>
2008-06-08 01:41:07 +0000

    Sun Jun  8 08:40:30 UTC 2008  James H. Hill  <hillj@isis.vanderbilt.edu>
    git-svn-id: https://svn.cs.iupui.edu/repos/CUTS/trunk@1940 9fe22f9e-a3cf-4311-a215-5c011952144b

--- CUTS/cuts/cuts_java_Logger.cpp
+++ CUTS/cuts/cuts_java_Logger.cpp
@@ -2,6 +2,7 @@
 
 #include "cuts_java_Logger.h"
 #include "ace/Log_Msg.h"
+#include <iostream>
 
 //
 // Java_cuts_java_Logger_log
@@ -12,35 +13,30 @@ Java_cuts_java_Logger_log (JNIEnv * env,
                            jint priority, 
                            jstring message)
 {
+  std::cerr << "Hello, World" << std::endl;
+
   // Extract the value of the priority. We need to convert
   // it into an ACE_Log_Priority value.
-  ACE_Log_Priority level = static_cast <ACE_Log_Priority> (priority);
+  // ACE_Log_Priority level = static_cast <ACE_Log_Priority> (priority);
 
-  if (level != -1)
-  {
-    // Extract the string message from the Java environment. We
-    // are not going to changes its formatting.
-    const char * msg = env->GetStringUTFChars (message, 0);
-
-    if (msg != 0)
+  // Extract the string message from the Java environment. We
+  // are not going to changes its formatting.
+  const char * msg = env->GetStringUTFChars (message, 0);
+  
+  if (msg != 0)
     {
       // Generate the log message. Should we use the macro, i.e., 
       // ACE_DEBUG or ACE_ERROR, or directly use ACE_LOG_MSG??
-      ACE_LOG_MSG->log (level, msg);
-
+      ACE_LOG_MSG->log (LM_INFO, msg);
+      
       // Release the extracted string.
       env->ReleaseStringUTFChars (message, msg);
     }
-    else
+  else
     {
       ACE_ERROR ((LM_ERROR,
                   "*** error: failed to extract log message\n"));
     }
-  }
-  else
-  {
-    ACE_ERROR ((LM_ERROR,
-                "*** error: failed to extract priority value\n"));
-  }
 }
+
 
