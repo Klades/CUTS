d0221eb12e56a7c5984d8548c43b59c6df7a2cdd
James H. Hill <hillj@cs.iupui.edu>
2008-06-26 19:17:25 +0000

    Thu Jun 26 19:16:01 UTC 2008  James H. Hill  <hillj@isis.vanderbilt.edu>
    git-svn-id: https://svn.cs.iupui.edu/repos/CUTS/trunk@1980 9fe22f9e-a3cf-4311-a215-5c011952144b

--- CUTS/bin/jbi/SingleProcess.sh
+++ CUTS/bin/jbi/SingleProcess.sh
@@ -20,7 +20,7 @@ JBI_CLASSPATH=${JBI_ROOT}/lib/capi1.5.jar:${JBI_ROOT}/lib/dom4j-1.6.1.jar:${JBI_
 CUTS_CLASSPATH=${CUTS_ROOT}/lib/cuts.java.jar:${CUTS_ROOT}/lib/cuts.java.jbi.jar:${CUTS_ROOT}/contrib/java/spring.jar:${CUTS_ROOT}/contrib/java/log4j-1.2.15.jar:${CUTS_ROOT}/contrib/java/commons-logging-1.1.1.jar
 
 # execute to CORBA application
-exec ${JAVA_CMD}                                                    \
+exec ${JAVA_CMD} ${JAVA_OPTS}                                       \
     -Dorg.omg.CORBA.ORBClass=org.jacorb.orb.ORB                     \
     -Dorg.omg.CORBA.ORBSingletonClass=org.jacorb.orb.ORBSingleton   \
     -classpath ${CUTS_CLASSPATH}:${JBI_CLASSPATH}:${CLASSPATH}      \
