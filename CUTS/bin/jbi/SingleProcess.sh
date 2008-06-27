9132f15d516c707b4641b94d7829218a3617fd1d
James H. Hill <hillj@cs.iupui.edu>
2008-06-27 00:23:53 +0000

    Fri Jun 27 07:20:41 UTC 2008  James H. Hill  <hillj@isis.vanderbilt.edu>
    git-svn-id: https://svn.cs.iupui.edu/repos/CUTS/trunk@1981 9fe22f9e-a3cf-4311-a215-5c011952144b

--- CUTS/bin/jbi/SingleProcess.sh
+++ CUTS/bin/jbi/SingleProcess.sh
@@ -17,7 +17,7 @@
 JAVA_CMD=java
 
 JBI_CLASSPATH=${JBI_ROOT}/lib/capi1.5.jar:${JBI_ROOT}/lib/dom4j-1.6.1.jar:${JBI_ROOT}/lib/jaxen-1.1.1.jar:${JBI_ROOT}/lib/jbossall-client.jar
-CUTS_CLASSPATH=${CUTS_ROOT}/lib/cuts.java.jar:${CUTS_ROOT}/lib/cuts.java.jbi.jar:${CUTS_ROOT}/contrib/java/spring.jar:${CUTS_ROOT}/contrib/java/log4j-1.2.15.jar:${CUTS_ROOT}/contrib/java/commons-logging-1.1.1.jar
+CUTS_CLASSPATH=${CUTS_ROOT}/lib/cuts.java.jar:${CUTS_ROOT}/lib/cuts.java.jbi.jar:${CUTS_ROOT}/contrib/java/spring.jar:${CUTS_ROOT}/contrib/java/log4j-1.2.15.jar:${CUTS_ROOT}/contrib/java/commons-logging-1.1.1.jar:${CUTS_ROOT}/contrib/java/castor-1.2.jar
 
 # execute to CORBA application
 exec ${JAVA_CMD} ${JAVA_OPTS}                                       \
