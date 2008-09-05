deccaf1a4ae39567c6cc023a6be6d8cfb67e0de2
James H. Hill <hillj@cs.iupui.edu>
2008-09-05 22:12:28 +0000

    Fri Sep  5 22:11:02 UTC 2008  James H. Hill  <hillj@isis.vanderbilt.edu>
    git-svn-id: https://svn.cs.iupui.edu/repos/CUTS/trunk@2176 9fe22f9e-a3cf-4311-a215-5c011952144b

--- CUTS/etc/emulab/generate-dance-nodemap.sh
+++ CUTS/etc/emulab/generate-dance-nodemap.sh
@@ -8,8 +8,11 @@
 # @author         James H. Hill
 #
 
+# save the command-line arguments
+PORT_NUMBER=$1
+
 # get the suffix for the hostname's alias
 HOSTNAME_SUFFIX=`cat /var/emulab/boot/nickname | awk -F '.' '{ printf "%s.%s", $2, $3 }'`
 
 # write the corbaloc for each host
-grep 'h' /var/emulab/boot/ltmap | awk -F ' ' '{ printf "%s    corbaloc:iiop:%s.%s.isislab.vanderbilt.edu:30000/NodeManager\n", $2, $2, "'"${HOSTNAME_SUFFIX}"'" }'
+grep 'h' /var/emulab/boot/ltmap | awk -F ' ' '{ printf "%s    corbaloc:iiop:%s.%s.isislab.vanderbilt.edu:"'"${PORT_NUMBER}"'"/NodeManager\n", $2, $2, "'"${HOSTNAME_SUFFIX}"'" }'
