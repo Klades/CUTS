ca5f801fd08de4026cd84adb3b3dd831266bc391
James H. Hill <hillj@cs.iupui.edu>
2008-12-24 21:44:11 +0000

    Wed Dec 24 21:43:38 UTC 2008  James H. Hill  <hillj@isis.vanderbilt.edu>
    git-svn-id: https://svn.cs.iupui.edu/repos/CUTS/trunk@2331 9fe22f9e-a3cf-4311-a215-5c011952144b

--- CUTS/utils/BMW/website/login.aspx.cs
+++ CUTS/utils/BMW/website/login.aspx.cs
@@ -20,8 +20,6 @@ using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
-using System.Web.UI.WebControls.WebParts;
-using System.Data.SQLite;
 
 public partial class Login : System.Web.UI.Page
 {
