e4ba87891f3df16598781f8f1ab8e2214029643b
James H. Hill <hillj@cs.iupui.edu>
2009-01-22 06:58:13 +0000

    Thu Jan 22 06:56:44 UTC 2009  James H. Hill  <hillj@isis.vanderbilt.edu>
    git-svn-id: https://svn.cs.iupui.edu/repos/CUTS/trunk@2412 9fe22f9e-a3cf-4311-a215-5c011952144b

---
+++ CUTS/examples/PICML/AntiSPAM/cpu-profile-generator.py
@@ -0,0 +1,101 @@
+# -*- Python -*-
+
+###############################################################################
+#
+# @file             cuts.analysis.py
+#
+# $Id$
+#
+# @author           James H. Hill
+#
+###############################################################################
+
+import sys
+import getopt
+import random
+import math
+
+#
+# main
+#
+def main ():
+  try:
+    short_opt = "n:"
+    long_opt = ["components=", "enable-post"]
+
+    opts, args = getopt.gnu_getopt (sys.argv[1:], short_opt, long_opt)
+
+  except getopt.GetoptError, err:
+    # print help information and exit
+    print str (err)
+    sys.exit (2)
+
+  count = 0
+  enable_post = False
+
+  # parse the command-line arguments
+  for o, a in opts:
+    if o in ("-n", "--components"):
+      count = a
+    elif o in ("--enable-post"):
+      enable_post = True
+    else:
+      assert False, "unhandled option (%s)" % o
+
+  assert count != 0, "number of instances not specified"
+
+  # seed the random number generator
+  random.seed (None)
+
+  # generate all the profiles
+  for i in range (1, int (count) + 1) :
+    generateCPUProfile (i, enable_post)
+
+#
+# generateCPUProfile
+#
+def generateCPUProfile (id, enable_post) :
+  # open the profile for writing
+  profile_name = "inst%d.cpu" % id
+  profile = open (profile_name, "w")
+
+  # write the service times for each port
+  for port in ['A', 'B', 'C', 'D', 'E'] :
+    # get pre/post service times
+    service_time = getRandomServiceTime ()
+
+    if (enable_post) :
+      pre, post = getRandomPrePostTimes (service_time)
+    else :
+      pre, post = service_time, 0
+
+    # write th service times to the profile
+    profile.write ("pre%c=%d\npost%c=%d\n" % (port, pre, port, post))
+
+  # close the profile
+  profile.close ()
+
+#
+# getRandomServiceTime
+#
+def getRandomServiceTime () :
+  return random.randint (10, 70)
+
+#
+# getRandomPrePostTimes
+#
+def getRandomPrePostTimes (service_time) :
+  # determine the preprocessing service time
+  pre = random.randint (min (10, service_time), min (service_time, 70))
+
+  # calculate the postprocessing service time
+  post = service_time - pre
+
+  # return the values to the caller
+  return pre, post;
+
+###############################################################################
+## main entry point
+
+if __name__ == "__main__" :
+  main ()
