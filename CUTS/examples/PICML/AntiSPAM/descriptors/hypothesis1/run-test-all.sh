b45c14eb74e797c187e76946d05dc6e26386f995
James H. Hill <hillj@cs.iupui.edu>
2009-01-24 21:31:36 +0000

    Sat Jan 24 21:30:58 UTC 2009  James H. Hill  <hillj@isis.vanderbilt.edu>
    git-svn-id: https://svn.cs.iupui.edu/repos/CUTS/trunk@2435 9fe22f9e-a3cf-4311-a215-5c011952144b

--- CUTS/examples/PICML/AntiSPAM/descriptors/hypothesis1/run-test-all.sh
+++ CUTS/examples/PICML/AntiSPAM/descriptors/hypothesis1/run-test-all.sh
@@ -3,9 +3,18 @@
 CONFIG_LIST=$1
 OUTPUT_PATH=$2
 
+PROJ_NAME=`${CUTS_ROOT}/etc/emulab/get-project-name.sh`
+EXPR_NAME=`${CUTS_ROOT}/etc/emulab/get-experiment-name.sh`
+NODE_LIST=`grep h /var/emulab/boot/ltmap | sed "s/h //"`
+
 for i in `cat ${CONFIG_LIST}`; do
   # run the test
   ./run-test.sh $i ${OUTPUT_PATH}
 
-  # reset the testing environment
+  for j in `${NODE_LIST}`; do
+    HOSTNAME=$j.${EXPR_NAME}.${PROJ_NAME}.isislab.vanderbilt.edu
+    
+    echo "*** info: sending reset signal to ${HOSTNAME}"
+    ${CUTS_ROOT}/bin/cutsnode --reset -ORBInitRef CUTS/NodeDaemon=${HOSTNAME}:30000/CUTS/NodeDaemon
+  done
 done
