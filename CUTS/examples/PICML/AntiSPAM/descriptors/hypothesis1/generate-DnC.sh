9c2e5f0738634b002e57cf1897ae5d74e1a8c9c0
James H. Hill <hillj@cs.iupui.edu>
2009-01-24 20:19:15 +0000

    Sat Jan 24 20:17:54 UTC 2009  James H. Hill  <hillj@isis.vanderbilt.edu>
    git-svn-id: https://svn.cs.iupui.edu/repos/CUTS/trunk@2433 9fe22f9e-a3cf-4311-a215-5c011952144b

---
+++ CUTS/examples/PICML/AntiSPAM/descriptors/hypothesis1/generate-DnC.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+CONFIG_LIST="$(ls *.cdp)"
+
+# iterate thru each configuration
+for i in `ls ${CONFIG_LIST}`; do
+  # initialize the configration variables
+  CONFIG_NUMBER=`echo $i | sed "s/hypothesis1-template-assembly//" | sed "s/.cdp//"`
+  CONFIG_TEMPLATE=hypothesis1-template-assembly${CONFIG_NUMBER}.cdp
+  CONFIG_OUTPUT_DIR=configuration${CONFIG_NUMBER}
+
+  # delete any of the existing configurations
+  if [ -d "${CONFIG_OUTPUT_DIR}" ]; then
+    rm -fr ${CONFIG_OUTPUT_DIR}
+  fi
+
+  # make the output directory
+  mkdir ${CONFIG_OUTPUT_DIR}
+
+  # generate the current configuration(s)
+  echo "*** info: generating $i into ${CONFIG_OUTPUT_DIR}"
+  $CUTS_ROOT/bin/cuts-template --template=${CONFIG_TEMPLATE} --config-list=hypothesis1-template-assembly.config -o ${CONFIG_OUTPUT_DIR} --use-env
+
+  # iterate thur each generated configuration
+  DEPLOYMENT_LIST=${CONFIG_OUTPUT_DIR}/*.cdp
+
+  for j in `ls ${DEPLOYMENT_LIST}`; do
+    # intialize the deployment variables
+    DEPLOYMENT_NUMBER=`echo "$j" | sed "s/configuration[0-9]*\/H1-DeploymentTemplate-//" | sed "s/.cdp//"`
+    DEPLOYMENT_OUTPUT_DIR=${CONFIG_OUTPUT_DIR}/deployment${DEPLOYMENT_NUMBER}
+
+    # delete any of the existing deployments
+    if [ -d "${DEPLOYMENT_OUTPUT_DIR}" ]; then
+      rm -fr ${DEPLOYMENT_OUTPUT_DIR}
+    fi
+
+    mkdir ${DEPLOYMENT_OUTPUT_DIR}
+
+    # generate the deployment(s)
+    echo "*** info: generating $j into ${DEPLOYMENT_OUTPUT_DIR}"
+    $CUTS_ROOT/bin/cuts-template --template=$j --config-list=unique-deployments.config -o ${DEPLOYMENT_OUTPUT_DIR} --use-env
+  done
+done
