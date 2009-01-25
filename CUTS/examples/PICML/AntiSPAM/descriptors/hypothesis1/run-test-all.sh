a3d85bd739232ee65a633485f3bbc67ee1845944
James H. Hill <hillj@cs.iupui.edu>
2009-01-25 15:39:46 +0000

    Sun Jan 25 15:39:15 UTC 2009  James H. Hill  <hillj@isis.vanderbilt.edu>
    git-svn-id: https://svn.cs.iupui.edu/repos/CUTS/trunk@2448 9fe22f9e-a3cf-4311-a215-5c011952144b

--- CUTS/examples/PICML/AntiSPAM/descriptors/hypothesis1/run-test-all.sh
+++ CUTS/examples/PICML/AntiSPAM/descriptors/hypothesis1/run-test-all.sh
@@ -4,9 +4,19 @@ CONFIG_LIST=$1
 OUTPUT_PATH=$2
 
 for i in `cat ${CONFIG_LIST}`; do
-  # run the test
-  ./run-test.sh $i ${OUTPUT_PATH}
+  DATAFILE=`echo "$i" | sed "s/\//./g"`
+  ARCHIVE_FILE=${OUTPUT_PATH}/${DATAFILE}
 
-  # reset each of the nodes
-  ./reset-nodes.sh 30000  
+  if [ ! -f "${ARCHIVE_FILE}" ]; then
+    # run the test
+    echo "*** info: running test for deployment $i; storing data at ${DATAFILE}"
+    ./run-test.sh $i ${DATAFILE}
+
+    # archive the datafile
+    mv ${DATAFILE} ${OUTPUT_PATH}
+    
+    # reset the test environment
+    ./reset-nodes.sh 30000
+    sleep 5s
+  fi
 done
--- CUTS/examples/PICML/AntiSPAM/descriptors/hypothesis1/run-test.sh
+++ CUTS/examples/PICML/AntiSPAM/descriptors/hypothesis1/run-test.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
 DEPLOYMENT=$1
-DATAFILE=`echo "$1" | sed "s/\//./g"`
-ARCHIVE_FILE=$2/${DATAFILE}
+DATAFILE=$2
 
-if [ ! -f "${ARCHIVE_FILE}" ]; then
-  # run the test
-  echo "*** info: running test for deployment ${DEPLOYMENT}; storing data at ${DATAFILE}"
-  ${CUTS_ROOT}/bin/cutstest --config=/proj/AntiSPAM/config/cutstest.config -DDEPLOYMENT_PLAN=${DEPLOYMENT} --time=120 -f ${DATAFILE} --ignore-errors --debug --verbose
+${CUTS_ROOT}/bin/cutstest --config=/proj/AntiSPAM/config/cutstest.config -DDEPLOYMENT_PLAN=${DEPLOYMENT} --time=120 -f ${DATAFILE} --ignore-errors --debug --verbose
 
-  # move the data file to the archive
-  mv ${DATAFILE} $2/${DATAFILE}
-else
-  echo "*** info: ${DATAFILE} already archived; skipping test..."
-fi
