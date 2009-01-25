fb5adad9cf14e44f5cc717f08accb442c47aec0b
James H. Hill <hillj@cs.iupui.edu>
2009-01-25 06:24:50 +0000

    Sun Jan 25 06:24:12 UTC 2009  James H. Hill  <hillj@isis.vanderbilt.edu>
    git-svn-id: https://svn.cs.iupui.edu/repos/CUTS/trunk@2446 9fe22f9e-a3cf-4311-a215-5c011952144b

--- CUTS/examples/PICML/AntiSPAM/descriptors/hypothesis1/run-test.sh
+++ CUTS/examples/PICML/AntiSPAM/descriptors/hypothesis1/run-test.sh
@@ -2,14 +2,15 @@
 
 DEPLOYMENT=$1
 DATAFILE=`echo "$1" | sed "s/\//./g"`
+ARCHIVE_FILE=$2/${DATAFILE}
 
-#if [ ! -f "${DATAFILE}" ]; then
+if [ ! -f "${ARCHIVE_FILE}" ]; then
   # run the test
   echo "*** info: running test for deployment ${DEPLOYMENT}; storing data at ${DATAFILE}"
   ${CUTS_ROOT}/bin/cutstest --config=/proj/AntiSPAM/config/cutstest.config -DDEPLOYMENT_PLAN=${DEPLOYMENT} --time=120 -f ${DATAFILE} --ignore-errors --debug --verbose
 
   # move the data file to the archive
   mv ${DATAFILE} $2/${DATAFILE}
-#else
-#  echo "*** info: ${DATAFILE} already exists; skipping test..."
-#fi
+else
+  echo "*** info: ${DATAFILE} already archived; skipping test..."
+fi
