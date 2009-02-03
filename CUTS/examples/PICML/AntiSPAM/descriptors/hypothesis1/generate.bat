4f3998caf152aac0c790f08a16f5b05d3837a5eb
James H. Hill <hillj@cs.iupui.edu>
2009-02-03 15:46:41 +0000

    Tue Feb  3 15:43:42 UTC 2009  James H. Hill  <hillj@isis.vanderbilt.edu>
    git-svn-id: https://svn.cs.iupui.edu/repos/CUTS/trunk@2469 9fe22f9e-a3cf-4311-a215-5c011952144b

--- CUTS/examples/PICML/AntiSPAM/descriptors/hypothesis1/generate.bat
+++ CUTS/examples/PICML/AntiSPAM/descriptors/hypothesis1/generate.bat
@@ -4,5 +4,6 @@ set ANTISPAM_DIR=%CUTS_ROOT%\examples\PICML\AntiSPAM
 %CUTS_ROOT%\bin\gmet2m-exec -p %ANTISPAM_DIR%\AntiSPAM.xme --target=ComponentImplementations/TemplateAssembly -f hypothesis1-template-assembly%1.assembly --parser=GME_T2M_PICML_Assembly_Parser
 %CUTS_ROOT%\bin\gmet2m-exec -p %ANTISPAM_DIR%\AntiSPAM.xme --target=DeploymentPlans -f hypothesis1-template-assembly.deployment --parser=GME_T2M_PICML_Deployment_Parser
 %CUTS_ROOT%\bin\gmet2m-exec -p %ANTISPAM_DIR%\AntiSPAM.xme --run=MGA.Interpreter.DeploymentPlan --run-output=%ANTISPAM_DIR%\descriptors\hypothesis1 --run-focus=DeploymentPlans/TemplateDeployment
+mv %ANTISPAM_DIR%\descriptors\hypothesis1\TemplateDeployment.cdp %ANTISPAM_DIR%\descriptors\hypothesis1\hypothesis1-template-assembly%1.cdp
 
 @endlocal
