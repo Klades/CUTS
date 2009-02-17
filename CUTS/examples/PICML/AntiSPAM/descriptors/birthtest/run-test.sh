dc240417a4903ce982670b2ab6929fcfabf89588
James H. Hill <hillj@cs.iupui.edu>
2009-02-17 00:34:00 +0000

    Tue Feb 17 00:33:49 UTC 2009  James H. Hill  <hillj@isis.vanderbilt.edu>
    git-svn-id: https://svn.cs.iupui.edu/repos/CUTS/trunk@2499 9fe22f9e-a3cf-4311-a215-5c011952144b

--- CUTS/examples/PICML/AntiSPAM/descriptors/birthtest/run-test.sh
+++ CUTS/examples/PICML/AntiSPAM/descriptors/birthtest/run-test.sh
@@ -3,5 +3,4 @@
 DEPLOYMENT=$1
 DATAFILE=$2
 
-${CUTS_ROOT}/bin/cutstest --config=/proj/AntiSPAM/config/cutstest.config -DDEPLOYMENT_PLAN=${DEPLOYMENT} --time=120 -f ${DATAFILE} --ignore-errors --debug --verbose --shutdown-timeout=5
-
+${CUTS_ROOT}/bin/cutstest --config=/proj/AntiSPAM/config/cutstest.birthtest.config -DDEPLOYMENT_PLAN=${DEPLOYMENT} --time=120 -f ${DATAFILE} --ignore-errors --debug --verbose --shutdown-timeout=5
