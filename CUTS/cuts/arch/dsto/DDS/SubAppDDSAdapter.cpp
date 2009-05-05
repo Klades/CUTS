e3027c3fa0f340e735d14e7d71fd79dff5fe7b00
Jeff Parsons <parsons@dre.vanderbilt.edu>
2009-05-05 19:18:53 +0000

    ChangeLogTag: Tue May  5 19:17:57 UTC 2009  Jeff Parsons <j.parsons@vanderbilt.edu>
    git-svn-id: https://svn.cs.iupui.edu/repos/CUTS/trunk@2612 9fe22f9e-a3cf-4311-a215-5c011952144b

--- CUTS/cuts/arch/dsto/DDS/SubAppDDSAdapter.cpp
+++ CUTS/cuts/arch/dsto/DDS/SubAppDDSAdapter.cpp
@@ -26,14 +26,16 @@ namespace DSTO_AppSpace_Impl
 //    ACE_NEW (this->context_, ::DSTO::ContextBase);
     
     try
-     {        
-        // TODO - generate a domain id.  
-        if (! this->dds_utility_.init (0))
-          {
-            ACE_ERROR ((LM_EMERGENCY,
-                        ACE_TEXT ("SubAppDDSAdapter: error ")
-                        ACE_TEXT ("in DDS initialization\n")));
-          }  
+     {
+       this->populate_consumer_table ();
+     
+       // TODO - generate a domain id.  
+       if (! this->dds_utility_.init (0))
+         {
+           ACE_ERROR ((LM_EMERGENCY,
+                       ACE_TEXT ("SubAppDDSAdapter: error ")
+                       ACE_TEXT ("in DDS initialization\n")));
+         }  
 
         ::Outer::TestData_DDSTypeSupport_var ts =
           new ::Outer::TestData_DDSTypeSupport;
