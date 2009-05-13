9c06d5601b1353e89f93e07cfd544d8ac95b0e36
James H. Hill <hillj@cs.iupui.edu>
2009-05-13 06:59:35 +0000

    Wed May 13 06:58:49 UTC 2009  James H. Hill  <hillj@isis.vanderbilt.edu>
    git-svn-id: https://svn.cs.iupui.edu/repos/CUTS/trunk@2643 9fe22f9e-a3cf-4311-a215-5c011952144b

--- CUTS/examples/arch/tcpip/HelloWorld/TCPIP_HelloWorldC.cpp
+++ CUTS/examples/arch/tcpip/HelloWorld/TCPIP_HelloWorldC.cpp
@@ -2,10 +2,7 @@
 
 #include "TCPIP_HelloWorldC.h"
 
-/////////////////////////////////////////////////////////////////////////////
-// ::TimeValue
-
-ACE_CDR::Boolean operator << (ACE_OutputCDR & stream, ::TimeValue & rhs)
+ACE_CDR::Boolean operator << (CUTS_TCPIP_OutputCDR & stream, ::TimeValue & rhs)
 {
   stream << rhs.sec;
   stream << rhs.usec;
@@ -21,10 +18,7 @@ ACE_CDR::Boolean operator >> (CUTS_TCPIP_InputCDR & stream, ::TimeValue & rhs)
   return stream.good_bit ();
 }
 
-/////////////////////////////////////////////////////////////////////////////
-// ::Message
-
-ACE_CDR::Boolean operator << (ACE_OutputCDR & stream, ::Message & rhs)
+ACE_CDR::Boolean operator << (CUTS_TCPIP_OutputCDR & stream, ::Message & rhs)
 {
   stream << rhs.time ();
   stream << rhs.content ();
@@ -34,10 +28,8 @@ ACE_CDR::Boolean operator << (ACE_OutputCDR & stream, ::Message & rhs)
 
 ACE_CDR::Boolean operator >> (CUTS_TCPIP_InputCDR & stream, ::Message & rhs)
 {
-  // extract the <time> value.
   stream >> rhs.time ();
 
-  // extract the <content> value.
   ACE_CString content;
   stream >> content;
   rhs.content (content.c_str ());
--- CUTS/examples/arch/tcpip/HelloWorld/TCPIP_HelloWorldC.h
+++ CUTS/examples/arch/tcpip/HelloWorld/TCPIP_HelloWorldC.h
@@ -4,20 +4,16 @@
 #define _TCPIP_HELLOWORLDC_H_
 
 #include "HelloWorldC.h"
+#include "cuts/arch/tcpip/TCPIP_InputCDR.h"
+#include "cuts/arch/tcpip/TCPIP_OutputCDR.h"
 #include "TCPIP_HelloWorld_stub_export.h"
 
-#include "cuts/arch/tcpip/TCPIP_Object_T.h"
-#include "cuts/arch/tcpip/TCPIP_Var_T.h"
-#include "cuts/arch/tcpip/TCPIP_InputCDR.h"
-#include "ace/SString.h"
-
 // Stream operations
-ACE_CDR::Boolean TCPIP_HELLOWORLD_STUB_Export operator << (ACE_OutputCDR & stream, ::TimeValue & rhs);
+ACE_CDR::Boolean TCPIP_HELLOWORLD_STUB_Export operator << (CUTS_TCPIP_OutputCDR & stream, ::TimeValue & rhs);
 ACE_CDR::Boolean TCPIP_HELLOWORLD_STUB_Export operator >> (CUTS_TCPIP_InputCDR & stream, ::TimeValue & rhs);
 
-
 // Stream operations
-ACE_CDR::Boolean TCPIP_HELLOWORLD_STUB_Export operator << (ACE_OutputCDR & stream, ::Message & rhs);
+ACE_CDR::Boolean TCPIP_HELLOWORLD_STUB_Export operator << (CUTS_TCPIP_OutputCDR & stream, ::Message & rhs);
 ACE_CDR::Boolean TCPIP_HELLOWORLD_STUB_Export operator >> (CUTS_TCPIP_InputCDR & stream, ::Message & rhs);
 
 #endif  // !defined _TCPIP_HELLOWORLDC_H_
