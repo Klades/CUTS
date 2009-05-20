718ce8ddd5a298d0c61f9f029b2218d76070e020
James H. Hill <hillj@cs.iupui.edu>
2009-05-20 06:33:52 +0000

    Wed May 20 06:32:58 UTC 2009  James H. Hill  <hillj@isis.vanderbilt.edu>
    git-svn-id: https://svn.cs.iupui.edu/repos/CUTS/trunk@2673 9fe22f9e-a3cf-4311-a215-5c011952144b

--- CUTS/examples/java/logging/client/log4j/HelloWorld.java
+++ CUTS/examples/java/logging/client/log4j/HelloWorld.java
@@ -6,17 +6,24 @@ public class HelloWorld
 {
   public static void main (String [] args)
   {
-    Logger logger = Logger.getLogger (HelloWorld.class);
-    logger.log (Level.DEBUG, "Hello, World!!");
+    try
+    {
+      Logger logger = Logger.getLogger(HelloWorld.class);
+      logger.log(Level.DEBUG, "Hello, World!!");
 
-    logger.log (Level.DEBUG, "This is message 1 before sleep at " + System.currentTimeMillis ());
+      logger.log(Level.DEBUG, "This is message 1 before sleep at " + System.currentTimeMillis());
 
-    // sleep for 5 seconds
-    Thread.sleep (5000);
+      // sleep for 5 seconds
+      Thread.sleep(5000);
 
-    logger.log (Level.DEBUG, "This is message 1 after sleep at " + System.currentTimeMillis ());
+      logger.log(Level.DEBUG, "This is message 1 after sleep at " + System.currentTimeMillis());
 
-    // Make sure the shutdown the log manager.
-    LogManager.shutdown ();
+      // Make sure the shutdown the log manager.
+      LogManager.shutdown();
+    }
+    catch (Exception ex)
+    {
+      ex.printStackTrace ();
+    }
   }
 }
