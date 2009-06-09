66445d22123330359509c94485e5acb5ce8e1b10
James H. Hill <hillj@cs.iupui.edu>
2009-06-09 01:38:06 +0000

    Tue Jun  9 01:37:17 UTC 2009  James H. Hill  <hillj@dre.vanderbilt.edu>
    git-svn-id: https://svn.cs.iupui.edu/repos/CUTS/trunk@2739 9fe22f9e-a3cf-4311-a215-5c011952144b

--- CUTS/performance-tests/DOA-2009/EISA/client.cpp
+++ CUTS/performance-tests/DOA-2009/EISA/client.cpp
@@ -5,8 +5,60 @@
 #include "ace/Singleton.h"
 #include "ace/High_Res_Timer.h"
 #include "ace/streams.h"
-#include "AnyTestC.h"
+#include "ace/OS_NS_unistd.h"
+#include "eisa/EINode_i.h"
 #include <sstream>
+#include "eisa/Software_Probe.h"
+#include "eisa/Software_Probe_Factory_T.h"
+#include "eisa/Software_Probe_Data_Packet.h"
+
+class Simple_Probe : public EISA_Software_Probe
+{
+public:
+  Simple_Probe (EISA::EINode_ptr parent)
+    : EISA_Software_Probe (parent, "E860E6F7-0A10-4F94-846D-8E99F4B250B1", "heartbeat", " ")
+  {
+    EISA_Software_Probe_Data_Packet * packet = 0;
+
+    ACE_NEW_THROW_EX (packet,
+                      EISA_Software_Probe_Data_Packet (this->uuid_),
+                      CORBA::NO_MEMORY ());
+
+    this->packet_.reset (packet);
+  }
+
+  virtual ~Simple_Probe (void)
+  {
+
+  };
+
+  void init (const char *)
+  {
+
+  }
+
+  void fini (void)
+  {
+
+  }
+
+  void handle_command (const char * command)
+  {
+
+  }
+
+  void send_value (size_t value)
+  {
+    this->packet_->reset (++ this->seqnum_, 0);
+    this->packet_->write_long (value);
+    this->einode_->send_data (this->packet_->stream ());
+  }
+
+private:
+  ACE_Auto_Ptr <EISA_Software_Probe_Data_Packet> packet_;
+
+  size_t seqnum_;
+};
 
 class Client
 {
@@ -28,11 +80,13 @@ public:
       this->orb_ = ::CORBA::ORB_init (argc, argv);
       this->parse_args (argc, argv);
 
-      ::CORBA::Object_var obj = this->orb_->resolve_initial_references ("AnyTest");
-      ::PerformanceTests::AnyTest_var anytest = ::PerformanceTests::AnyTest::_narrow (obj.in ());
+      EISA_EINode_i einode (true);
+      einode.activate (this->config_file_, this->orb_.in ());
 
-      ::CORBA::Any any;
+      ACE_OS::sleep (2);
+
       size_t event_count (0);
+      Simple_Probe simple_probe (&einode);
 
       ACE_DEBUG ((LM_DEBUG,
                   "%T - %M - running the test for %d event(s)\n",
@@ -41,12 +95,9 @@ public:
       ACE_High_Res_Timer timer;
       timer.start ();
 
-      PerformanceTests::ComplexData complex_data;
-
-      for (; event_count < this->max_events_; ++ event_count)
+      while (event_count < this->max_events_)
       {
-        any <<= complex_data;
-        anytest->send_any (any);
+        simple_probe.send_value (++ event_count);
       }
 
       ACE_DEBUG ((LM_DEBUG,
@@ -54,6 +105,7 @@ public:
                   this->max_events_));
 
       timer.stop ();
+      einode.deactivate ();
 
       ACE_Time_Value elapsed_time;
       timer.elapsed_time (elapsed_time);
@@ -78,7 +130,7 @@ public:
 
   int parse_args (int argc, char * argv [])
   {
-    const char * optargs = "n:";
+    const char * optargs = "n:c:";
     ACE_Get_Opt get_opt (argc, argv, optargs);
 
     char opt;
@@ -88,8 +140,14 @@ public:
       switch (opt)
       {
       case 'n':
-        std::istringstream istr (get_opt.opt_arg ());
-        istr >> this->max_events_;
+        {
+          std::istringstream istr (get_opt.opt_arg ());
+          istr >> this->max_events_;
+        }
+        break;
+
+      case 'c':
+        this->config_file_ = get_opt.opt_arg ();
         break;
       }
     }
@@ -107,6 +165,8 @@ private:
   ::CORBA::ORB_var orb_;
 
   size_t max_events_;
+
+  ACE_CString config_file_;
 };
 
 #define CLIENT \
@@ -118,6 +178,11 @@ private:
 int ACE_TMAIN (int argc, char * argv [])
 {
   int retval;
+
+  u_long default_mask =
+    LM_EMERGENCY | LM_ALERT | LM_CRITICAL | LM_ERROR | LM_WARNING | LM_NOTICE | LM_DEBUG;
+
+  ACE_LOG_MSG->priority_mask (default_mask, ACE_Log_Msg::PROCESS);
 
   try
   {
