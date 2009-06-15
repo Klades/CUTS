feaae10ccfa102a536e0fdbc90a58f5c57ef38d7
James H. Hill <hillj@cs.iupui.edu>
2009-06-15 14:05:14 +0000

    Mon Jun 15 14:04:29 UTC 2009  James H. Hill  <hillj@dre.vanderbilt.edu>
    git-svn-id: https://svn.cs.iupui.edu/repos/CUTS/trunk@2761 9fe22f9e-a3cf-4311-a215-5c011952144b

--- CUTS/examples/arch/chaos/SimpleExample/OpenSplice_SimpleComponentC.h
+++ CUTS/examples/arch/chaos/SimpleExample/OpenSplice_SimpleComponentC.h
@@ -4,6 +4,8 @@
 #define _OPENSPLICE_SIMPLECOMPONENTC_H_
 
 #include "SimpleComponentC.h"
+#include "cuts/arch/opensplice/OpenSplice_Traits_T.h"
+#include "ddstypes/ModelDDSData_DDSDcps_impl.h"
 
 namespace Example
 {
