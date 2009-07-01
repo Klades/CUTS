b1f0f1fb8b20234209ff4d757dbdbaa331ee31b4
Pooja Varshneya <pooja.varshneya@vanderbilt.edu>
2009-07-01 18:10:00 +0000

    Wed Jul  1 16:25:35 UTC 2009  Pooja Varshneya  <varshneya_p@local>
    git-svn-id: https://svn.cs.iupui.edu/repos/CUTS/trunk@2799 9fe22f9e-a3cf-4311-a215-5c011952144b

---
+++ CUTS/tests/cuts/utils/Node_Daemon/TestApp1.cpp
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+#include <iostream>
+
+int main ()
+{
+  std::cout << "Hello from TestApp1\n";
+  sleep (2);
+  std::cout << "Bye from TestApp1\n";
+}
---
+++ CUTS/tests/cuts/utils/Node_Daemon/TestApp2.cpp
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include <iostream>
+
+int main ()
+{
+  std::cout << "Hello from TestApp2\n";
+  sleep (2);
+  std::cout << "Bye from TestApp2\n";
+}
