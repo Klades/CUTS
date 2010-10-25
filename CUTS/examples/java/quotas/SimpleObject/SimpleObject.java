71f375e4018c79628dc94e440476b1cfe1b2a10d
James H. Hill <hillj@cs.iupui.edu>
2010-10-25 18:34:57 +0000

    Mon Oct 25 18:33:43 UTC 2010  James H. Hill  <hillj at cs dot iupui dot edu>
    git-svn-id: https://svn.cs.iupui.edu/repos/CUTS/trunk@3019 9fe22f9e-a3cf-4311-a215-5c011952144b

---
+++ CUTS/examples/java/quotas/SimpleObject/SimpleObject.java
@@ -0,0 +1,29 @@
+// -*- Java -*-
+
+/**
+ * @class SimpleObject
+ *
+ * This is a simple class to illustrate QUOTAS capabilities.
+ */
+class SimpleObject
+{
+  public SimpleObject ()
+  {
+
+  }
+
+  public void printHelloWorld ()
+  {
+    System.out.println ("Hello, World!!");
+  }
+
+  public void printMessage (String msg)
+  {
+    System.out.println (msg);
+  }
+
+  public long addNumbers (long n1, long n2)
+  {
+    return n1 + n2;
+  }
+}
