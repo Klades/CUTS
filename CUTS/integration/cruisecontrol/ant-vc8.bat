e77fd5d464886561d610c84129c463abaeb9db50
James H. Hill <hillj@cs.iupui.edu>
2010-10-28 04:14:31 +0000

    Thu Oct 28 04:12:30 UTC 2010  James H. Hill  <hillj at cs dot iupui dot edu>
    git-svn-id: https://svn.cs.iupui.edu/repos/CUTS/trunk@3025 9fe22f9e-a3cf-4311-a215-5c011952144b

--- CUTS/integration/cruisecontrol/ant-vc8.bat
+++ CUTS/integration/cruisecontrol/ant-vc8.bat
@@ -2,13 +2,13 @@
 @setlocal
 
 @rem *** make sure Visual Studio 2005 environment variables are set
-call %VS80COMNTOOLS%\vsvars32.bat
+@call "%VS80COMNTOOLS%vsvars32.bat"
 
 @rem *** call the configuration script for Visual Studio 2005 builds
 @rem *** if and only if the script has been defined.
 @if defined VC80BUILDCFG call %VC80BUILDCFG%
 
 @rem *** run ant script
-call %ANT_HOME\ant.bat %*
+@call "%ANT_HOME%\bin\ant.bat" %*
 
 @endlocal
--- CUTS/integration/cruisecontrol/ant-vc9.bat
+++ CUTS/integration/cruisecontrol/ant-vc9.bat
@@ -2,13 +2,13 @@
 @setlocal
 
 @rem *** make sure Visual Studio 2008 environment variables are set
-call %VS90COMNTOOLS%\vsvars32.bat
+@call "%VS90COMNTOOLS%vsvars32.bat"
 
 @rem *** call the configuration script for Visual Studio 2008 builds
 @rem *** if and only if the script has been defined.
 @if defined VC90BUILDCFG call %VC90BUILDCFG%
 
 @rem *** run ant script
-call %ANT_HOME\ant.bat %*
+@call "%ANT_HOME%\bin\ant.bat" %*
 
 @endlocal
