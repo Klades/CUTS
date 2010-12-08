c77178e74152f0ff2c078d1751076478386f72a2
James H. Hill <hillj@cs.iupui.edu>
2010-12-08 06:25:37 +0000

    Wed Dec  8 06:24:32 UTC 2010  James H. Hill  <hillj at cs dot iupui dot edu>
    git-svn-id: https://svn.cs.iupui.edu/repos/CUTS/trunk@3062 9fe22f9e-a3cf-4311-a215-5c011952144b

--- CUTS/bin/quotas/PojoComponentServer.bat
+++ CUTS/bin/quotas/PojoComponentServer.bat
@@ -14,10 +14,11 @@
 @set CONTRIB_ROOT=%CUTS_ROOT%\contrib\java
 
 @rem ## set the classpath
-@set CLASSPATH=%CLASSPATH%;%CUTS_ROOT%\lib\cuts.quotas.pojo.jar
-@rem set CLASSPATH=%CLASSPATH%;%CONTRIB_ROOT%\spring.jar
-@rem set CLASSPATH=%CLASSPATH%;%CONTRIB_ROOT%\log4j-1.2.15.jar
-@rem set CLASSPATH=%CLASSPATH%;%CONTRIB_ROOT%\commons-logging-1.1.1.jar
+@set CLASSPATH=%CLASSPATH%;%CUTS_ROOT%\lib\cuts.quotas.pojo.jar;
+@set CLASSPATH=%CLASSPATH%;%CUTS_ROOT%\lib\cuts.datamodel.jar
+@set CLASSPATH=%CLASSPATH%;%CONTRIB_ROOT%\spring.jar
+@set CLASSPATH=%CLASSPATH%;%CONTRIB_ROOT%\log4j-1.2.15.jar
+@set CLASSPATH=%CLASSPATH%;%CONTRIB_ROOT%\commons-logging-1.1.1.jar
 
 @rem ## start the Java process
 java -Djava.endorsed.dirs=%CUTS_ROOT%\contrib\java -cp %CLASSPATH% cuts.quotas.pojo.PojoComponentServer %*
