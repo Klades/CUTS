f0fafa3375287f870445b41bd355b986c38870a0
James H. Hill <hillj@cs.iupui.edu>
2011-04-19 18:09:00 +0000

    Tue Apr 19 18:01:53 UTC 2011  James H. Hill  <hillj at cs dot iupui dot edu>
    git-svn-id: https://svn.cs.iupui.edu/repos/CUTS/trunk@3104 9fe22f9e-a3cf-4311-a215-5c011952144b

---
+++ CUTS/examples/tutorials/QuickStart/PingPong_PingComponentImpl.h
@@ -0,0 +1,64 @@
+// -*- C++ -*-
+
+#ifndef _PINGPONG_PINGCOMPONENTIMPL_H_
+#define _PINGPONG_PINGCOMPONENTIMPL_H_
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+#pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "ace/pre.h"
+#include "PingPongEC.h"
+#include "cuts/arch/ccm/CCM_Component_T.h"
+
+#include "cuts/Periodic_Event_T.h"
+
+namespace PingPong_PingComponentImpl
+{
+  // Type definition of the implentation base type
+  typedef CUTS_CCM_Component_T < CIAO_PingPong_PingComponent_Impl::PingComponent_Exec, ::PingPong::CCM_PingComponent_Context > PingComponent_Base;
+
+  /**
+   * @class PingComponent
+   *
+   * Implementation of the CIAO_PingPong_PingComponent_Impl::PingComponent_Exec component executor
+   */
+  class PingComponent :
+    public PingComponent_Base
+  {
+    public:
+    // Type definition of the base component type
+    typedef PingComponent_Base base_type;
+
+    // Default constructor
+    PingComponent (void);
+
+    // Destructor
+    virtual ~PingComponent (void);
+
+    // PeriodicEvent: eventProducer
+    void periodic_eventProducer (void);
+
+    private:
+    // variable: eventCount
+    ::CORBA::ULong eventCount_;
+
+    // periodic: eventProducer
+    CUTS_Periodic_Event_T < PingComponent > periodic_eventProducer_;
+  };
+}
+
+#include "PingPong_PingComponent_impl_export.h"
+
+//
+// create_PingPong_PingComponent_Impl
+//
+extern "C" PINGPONG_PINGCOMPONENT_IMPL_Export
+::Components::EnterpriseComponent_ptr create_PingPong_PingComponent_Impl (void);
+
+#include "ace/post.h"
+
+#endif  // !defined _PINGPONG_PINGCOMPONENTIMPL_H_
+
+// end of auto-generated file
+
---
+++ CUTS/examples/tutorials/QuickStart/PingPong_PongComponentImpl.h
@@ -0,0 +1,58 @@
+// -*- C++ -*-
+
+#ifndef _PINGPONG_PONGCOMPONENTIMPL_H_
+#define _PINGPONG_PONGCOMPONENTIMPL_H_
+
+#if !defined (ACE_LACKS_PRAGMA_ONCE)
+#pragma once
+#endif /* ACE_LACKS_PRAGMA_ONCE */
+
+#include "ace/pre.h"
+#include "PingPongEC.h"
+#include "cuts/arch/ccm/CCM_Component_T.h"
+
+
+namespace PingPong_PongComponentImpl
+{
+  // Type definition of the implentation base type
+  typedef CUTS_CCM_Component_T < CIAO_PingPong_PongComponent_Impl::PongComponent_Exec, ::PingPong::CCM_PongComponent_Context > PongComponent_Base;
+
+  /**
+   * @class PongComponent
+   *
+   * Implementation of the CIAO_PingPong_PongComponent_Impl::PongComponent_Exec component executor
+   */
+  class PongComponent :
+    public PongComponent_Base
+  {
+    public:
+    // Type definition of the base component type
+    typedef PongComponent_Base base_type;
+
+    // Default constructor
+    PongComponent (void);
+
+    // Destructor
+    virtual ~PongComponent (void);
+
+    // sink: recvData
+    virtual void push_recvData (::PingPong::SimpleEvent * ev);
+
+    private:
+  };
+}
+
+#include "PingPong_PongComponent_impl_export.h"
+
+//
+// create_PingPong_PongComponent_Impl
+//
+extern "C" PINGPONG_PONGCOMPONENT_IMPL_Export
+::Components::EnterpriseComponent_ptr create_PingPong_PongComponent_Impl (void);
+
+#include "ace/post.h"
+
+#endif  // !defined _PINGPONG_PONGCOMPONENTIMPL_H_
+
+// end of auto-generated file
+
