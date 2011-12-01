fc8427f283821e2613aa8516056c85510043e9aa
James H. Hill <hillj@cs.iupui.edu>
2011-12-01 16:10:20 +0000

    Thu Dec  1 16:09:21 UTC 2011  James H. Hill  <hillj at cs dot iupui dot edu>
    git-svn-id: https://svn.cs.iupui.edu/repos/CUTS/trunk@3231 9fe22f9e-a3cf-4311-a215-5c011952144b

--- cuts/config.h
+++ cuts/config.h
@@ -16,30 +16,6 @@
 #ifndef _CUTS_CONFIG_H_
 #define _CUTS_CONFIG_H_
 
-#include "ace/config-all.h"
-
-#if defined (__CUTS_INLINE__)
-# define CUTS_INLINE inline
-#else
-# define CUTS_INLINE
-#endif
-
-#define CUTS_DEFAULT_PORT                 3306
-
-#define CUTS_DEFAULT_EVENT_COUNT          20
-
-#define CUTS_EVENT_HANDLER_THREAD_COUNT   1
-
-#define CUTS_UNKNOWN_IMPL                 1
-
-#define CUTS_DEFAULT_LOG_SIZE             50
-
-#define CUTS_UNKNOWN_ID                   -1
-
-#define CUTS_RESULT_DEFAULT_BUFFER_SIZE   1024
-
-#if defined (CUTS_HAS_DOXYGEN)
-#include "config-doxygen.h"
-#endif
+#include "cuts/config-common.h"
 
 #endif  // !defined _CUTS_CONFIG_H_
