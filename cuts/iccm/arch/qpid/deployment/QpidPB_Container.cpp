3494073623f8db4f2cec5e1679e2193962df45de
Dennis Feiock <dfeiock@iupui.edu>
2015-05-18 14:05:59 -0400

    Renamed qpid architecture to qpidpb.

--- cuts/iccm/arch/qpid/deployment/QpidPB_Container.cpp
+++
@@ -1,7 +0,0 @@
-// $Id$
-
-#include "QpidPB_Container.h"
-
-#if !defined (__CUTS_INLINE__)
-#include "QpidPB_Container.inl"
-#endif
--- cuts/iccm/arch/qpid/servant/QpidPB_EventConsumer.cpp
+++
@@ -1,7 +0,0 @@
-// $IdQpidPB_CCM_EventConsumer.cpp 3030 2010-11-05 03:44:54Z hillj $
-
-#include "QpidPB_EventConsumer.h"
-
-#if !defined (__CUTS_INLINE__)
-#include "QpidPB_EventConsumer.inl"
-#endif
